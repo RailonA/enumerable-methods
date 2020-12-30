45c72c92e494df529fd0d0f5d621eb5aeef93b43
Railon Acosta <cagedtreestechnology@gmail.com>
2020-12-30 12:52:20 -0500

    Fix the comments

--- enumerable.rb
+++ enumerable.rb
@@ -1,5 +1,5 @@
 module Enumerable
-  # each
+  #1. each
   def my_each
     self_length = length
     self_length.times do |index|
@@ -7,7 +7,7 @@ module Enumerable
     end
   end
 
-  # my_each_width_index
+  #2. my_each_width_index
   def my_each_width_index
     self_length = length
     self_length.times do |index|
@@ -15,7 +15,7 @@ module Enumerable
     end
   end
 
-  # my_select
+  #3. my_select
   def my_select
     new_array = []
     my_each do |item|
@@ -24,7 +24,7 @@ module Enumerable
     new_array
   end
 
-  # my_all?
+  #4. my_all?
   def my_all?
     return false if empty?
 
@@ -34,7 +34,7 @@ module Enumerable
     true
   end
 
-  # my_any?
+  #5. my_any?
   def my_any?
     return false if empty?
 
@@ -44,7 +44,7 @@ module Enumerable
     true
   end
 
-  # my_none?
+  #6. my_none?
   def my_none?
     return false if empty?
 
@@ -54,7 +54,7 @@ module Enumerable
     true
   end
 
-  # my_count
+  #7. my_count
   def my_count
     item = nil
     count = 0
@@ -68,7 +68,7 @@ module Enumerable
     count
   end
 
-  #  my_map
+  #8.  my_map
   def my_map
     block = nil
     new_array = []
@@ -83,7 +83,7 @@ module Enumerable
     new_array
   end
 
-  # inject
+  #9. inject
   def my_inject
     result = self[0]
     my_each_width_index do |item, index|
@@ -95,43 +95,45 @@ module Enumerable
   end
 end
 
+#10. multiply_els
 def multiply_els(array)
   array.my_inject { |product, elem| product * elem }
 end
+
 # The Test array that I use to test the methods below
 # test_array = [4, 8, 2, 25, 2, 1, 100]
 
-# each
+#1. each
 # test_array.each {|item| puts item*2}
 # test_array.my_each {|item| puts item*2}
 
-# each_with_index
+#2. each_with_index
 # test_array.each_with_index {|item,i| puts "Index: #{i} Item: #{item}"}
 # test_array.my_each_width_index {|item,i| puts "Index: #{i} Item: #{item}"}
 
-# select
+#3. select
 # print test_array.select {|item| item.even? }
 # print test_array.my_select {|item| item.even? }
 
-# all?
+#4. all?
 # print test_array.all? {|item| item.is_a? Integer}
 # print test_array.all? {|num| item.is_a? String}
 # print test_array.my_all? {|item| item.is_a? Integer}
 # print test_array.my_all? {|item| item.is_a? String}
 
-# any?
+#5. any?
 # print test_array.any? {|item| item.is_a? Integer}
 # print test_array.any? {|item| item.is_a? String}
 # print test_array.my_any? {|item| item.is_a? Integer}
 # print test_array.my_any? {|item| item.is_a? String}
 
-# none?
+#6. none?
 # print test_array.none? {|item| item.is_a? String}
 # print test_array.none? {|item| item.is_a? Integer}
 # print test_array.my_none? {|item| item.is_a? String}
 # print test_array.my_none? {|item| item.is_a? Integer}
 
-# count
+#7. count
 # print test_array.count {|item| item < 4}
 # print test_array.count(5)
 # print test_array.count
@@ -139,21 +141,20 @@ end
 # print test_array.my_count(5)
 # print test_array.my_count
 
-# map
+#8. map
 # test_block = Proc.new {|elem| elem * 2}
 # print test_array.map {|item| item * 2 }
 # print test_array.map(&test_block)
 # print test_array.my_map {|item| item * 2 }
 # print test_array.my_map(&test_block)
 
-# inject
+#9. inject
 # print test_array.inject {|result,elem| result + elem}
 # print test_array.my_inject {|result,elem| result + elem}
 
-#  multiply_els
+#10.  multiply_els
 # def multiply_els(array)
 #   puts array.inject {|result,elem| result*elem }
 #   puts array.my_inject {|result,elem| result*elem }
 # end
-
-# print multiply_els(test_array)
+#  print multiply_els(test_array)
