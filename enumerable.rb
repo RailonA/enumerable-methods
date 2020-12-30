7bdbc618ce5b21e800b1d34da52b0e00b78339a8
Railon Acosta <cagedtreestechnology@gmail.com>
2020-12-30 12:15:16 -0500

    Fixed all linters

--- enumerable.rb
+++ enumerable.rb
@@ -1,146 +1,159 @@
-
-
 module Enumerable
-  #each
+  # each
   def my_each
-    self_length = self.length
-     self_length.times do |index|
+    self_length = length
+    self_length.times do |index|
       yield(self[index])
     end
   end
 
   # my_each_width_index
   def my_each_width_index
-      self_length = self.length
-      self_length.times do |index|
-        yield(self[index], index)
-      end
+    self_length = length
+    self_length.times do |index|
+      yield(self[index], index)
+    end
   end
 
   # my_select
   def my_select
-    new_array = Array.new
-    self.my_each do |item|
-      if yield(item)
-        new_array.push(item)
-      end
+    new_array = []
+    my_each do |item|
+      new_array.push(item) if yield(item)
     end
     new_array
   end
 
   # my_all?
   def my_all?
-    return false if self.empty?
-    self.my_each do |item|
-     unless yield(item)
-      return false 
-      end
+    return false if empty?
+
+    my_each do |item|
+      return false unless yield(item)
     end
     true
   end
 
-    # my_any?
-    def my_any?
-      return false if self.empty?
-      self.my_each do |item|
-       unless yield(item)
-        return false 
-        end
-      end
-      true
-    end
-  
-    # my_none?
-    def my_none?
-      return false if self.empty?
-      self.my_each do |item|
-       if yield(item)
-        return false 
-        end
-      end
-      true
-    end
-      
-    # my_count
-    def my_count
-      item = nil
-      count = 0
-        if item 
-          self.my_each {|elem| count += 1 if elem == item}
-        elsif block_given?
-          self.my_each {|elem| count += 1 if yield(elem)}
-        else
-          count = self.length
-        end 
-      count
-    end
+  # my_any?
+  def my_any?
+    return false if empty?
 
-    #  my_map
-    def my_map
-       block = nil
-       new_array = Array.new
-       if block
-        self.my_each_width_index do
-          |elem, index| new_array[index] = block.call(elem) 
-        end
-      else  self.my_each_width_index do 
-        |elem, index| new_array[index] = yield(elem)
+    my_each do |item|
+      return false unless yield(item)
+    end
+    true
+  end
+
+  # my_none?
+  def my_none?
+    return false if empty?
+
+    my_each do |item|
+      return false if yield(item)
+    end
+    true
+  end
+
+  # my_count
+  def my_count
+    item = nil
+    count = 0
+    if item
+      my_each { |elem| count += 1 if elem == item }
+    elsif block_given?
+      my_each { |elem| count += 1 if yield(elem) }
+    else
+      count = length
+    end
+    count
+  end
+
+  #  my_map
+  def my_map
+    block = nil
+    new_array = []
+    if block
+      my_each_width_index do |elem, index|
+        new_array[index] = block.call(elem)
       end
+    else my_each_width_index do |elem, index|
+           new_array[index] = yield(elem)
+         end
     end
     new_array
+  end
+
+  # inject
+  def my_inject
+    result = self[0]
+    my_each_width_index do |item, index|
+      next if index.zero?
+
+      result = yield(result, item)
     end
-
-
-    #  my_inject
-  
+    result
+  end
 end
-  
-  
-  test_arr = [100,54,1,2,3,4]
-  
+
+def multiply_els(array)
+  array.my_inject { |product, elem| product * elem }
+end
+# The Test array that I use to test the methods below
+# test_array = [4, 8, 2, 25, 2, 1, 100]
+
 # each
-# test_arr.each {|item| puts item*2}
-# test_arr.my_each {|item| puts item*2}
-  
+# test_array.each {|item| puts item*2}
+# test_array.my_each {|item| puts item*2}
+
 # each_with_index
-# test_arr.each_with_index {|item,i| puts "Index: #{i} Item: #{item}"}
-# test_arr.my_each_width_index {|item,i| puts "Index: #{i} Item: #{item}"}
-  
+# test_array.each_with_index {|item,i| puts "Index: #{i} Item: #{item}"}
+# test_array.my_each_width_index {|item,i| puts "Index: #{i} Item: #{item}"}
+
 # select
-# print test_arr.select {|item| item.even? }
-# print test_arr.my_select {|item| item.even? }
-  
+# print test_array.select {|item| item.even? }
+# print test_array.my_select {|item| item.even? }
+
 # all?
-# print test_arr.all? {|item| item.is_a? Integer}
-# print test_arr.all? {|num| item.is_a? String}
-# print test_arr.my_all? {|item| item.is_a? Integer}
-# print test_arr.my_all? {|item| item.is_a? String}
+# print test_array.all? {|item| item.is_a? Integer}
+# print test_array.all? {|num| item.is_a? String}
+# print test_array.my_all? {|item| item.is_a? Integer}
+# print test_array.my_all? {|item| item.is_a? String}
 
 # any?
-# print test_arr.any? {|item| item.is_a? Integer}
-# print test_arr.any? {|item| item.is_a? String}
-# print test_arr.my_any? {|item| item.is_a? Integer}
-# print test_arr.my_any? {|item| item.is_a? String}
-  
+# print test_array.any? {|item| item.is_a? Integer}
+# print test_array.any? {|item| item.is_a? String}
+# print test_array.my_any? {|item| item.is_a? Integer}
+# print test_array.my_any? {|item| item.is_a? String}
+
 # none?
-# print test_arr.none? {|item| item.is_a? String}
-# print test_arr.none? {|item| item.is_a? Integer}
-# print test_arr.my_none? {|item| item.is_a? String}
-# print test_arr.my_none? {|item| item.is_a? Integer}
+# print test_array.none? {|item| item.is_a? String}
+# print test_array.none? {|item| item.is_a? Integer}
+# print test_array.my_none? {|item| item.is_a? String}
+# print test_array.my_none? {|item| item.is_a? Integer}
 
 # count
-# print test_arr.count {|item| item < 4}
-# print test_arr.count(5)
-# print test_arr.count
-# print test_arr.my_count {|item| item < 4}
-# print test_arr.my_count(5) 
-# print test_arr.my_count
-  
+# print test_array.count {|item| item < 4}
+# print test_array.count(5)
+# print test_array.count
+# print test_array.my_count {|item| item < 4}
+# print test_array.my_count(5)
+# print test_array.my_count
+
 # map
-# print test_arr.map {|item| item * 10 }
-# print test_arr.map(&test_block)
-# print test_arr.my_map {|item| item * 10 }
-# print test_arr.my_map(&test_block)
+# test_block = Proc.new {|elem| elem * 2}
+# print test_array.map {|item| item * 2 }
+# print test_array.map(&test_block)
+# print test_array.my_map {|item| item * 2 }
+# print test_array.my_map(&test_block)
 
+# inject
+# print test_array.inject {|result,elem| result + elem}
+# print test_array.my_inject {|result,elem| result + elem}
 
- 
+#  multiply_els
+# def multiply_els(array)
+#   puts array.inject {|result,elem| result*elem }
+#   puts array.my_inject {|result,elem| result*elem }
+# end
+
+# print multiply_els(test_array)
