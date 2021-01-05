8d7c0ed3ff73802c4e3b02fc558c4a3cae36ef1c
RailonA <railonacosta@gmail.com>
2021-01-05 11:34:40 -0500

    Fix my_each to pass all of the new test I was given in the last code review.

--- enumerable.rb
+++ enumerable.rb
@@ -1,22 +1,118 @@
 module Enumerable
-  # 1. each
+  #1. each
 
-  # 1. each
-  def my_each
-    return self.to_enum unless block_given?
-
-    arr = to_a
-    (0..(arr.length - 1)).each do |i|
-      yield(arr[i])
+  
+      # self(index, index)
+      def my_each
+        return to_enum(:my_each) unless block_given?
+    
+        index = 0
+        while index < size
+          if is_a?(Array)
+            yield(self[index])
+          elsif is_a?(Range)
+            yield(to_a[index])
+          end
+          index += 1
+        end
+      if is_a?(Hash)
+           yield(keys.zip(values))
+      end
     end
-    self
+    end
+
+  #2. my_each_width_index
+  def my_each_width_index
+    self_length = length
+    self_length.times do |index|
+      yield(self[index], index)
+    end
   end
 
-end
-# 10. multiply_els
+  #3. my_select
+  def my_select
+    new_array = []
+    my_each do |item|
+      new_array.push(item) if yield(item)
+    end
+    new_array
+  end
+
+  #4. my_all?
+  def my_all?
+    return false if empty?
+
+    my_each do |item|
+      return false unless yield(item)
+    end
+    true
+  end
+
+  #5. my_any?
+  def my_any?
+    return false if empty?
+
+    my_each do |item|
+      return false unless yield(item)
+    end
+    true
+  end
+
+  #6. my_none?
+  def my_none?
+    return false if empty?
+
+    my_each do |item|
+      return false if yield(item)
+    end
+    true
+  end
+
+  #7. my_count
+  def my_count
+    item = nil
+    count = 0
+    if item
+      my_each { |elem| count += 1 if elem == item }
+    elsif block_given?
+      my_each { |elem| count += 1 if yield(elem) }
+    else
+      count = length
+    end
+    count
+  end
+
+  #8.  my_map
+  def my_map
+    block = nil
+    new_array = []
+    if block
+      my_each_width_index do |elem, index|
+        new_array[index] = block.call(elem)
+      end
+    else my_each_width_index do |elem, index|
+           new_array[index] = yield(elem)
+         end
+    end
+    new_array
+  end
+
+  #9. inject
+  def my_inject
+    result = self[0]
+    my_each_width_index do |item, index|
+      next if inde          index += 1
+      endld(result, item)
+     end
+    result
+   end
+
+
+#10. multiply_els
 def multiply_els(array)
   array.my_inject { |product, elem| product * elem }
 end
+
 
 # The Test array that I use to test the methods below
 ARRAY_SIZE = 100
@@ -30,7 +126,7 @@ block = proc { |num| num < (LOWEST_VALUE + HIGHEST_VALUE) / 2 }
 words = %w[dog door rod blade]
 #  passed
 range = Range.new(5, 50)
-#  passed
+#  failed
 hash = { a: 1, b: 2, c: 3, d: 4, e: 5 }
 # passed
 numbers = [1, 2i, 3.14]
@@ -39,8 +135,8 @@ array_clone = array.clone
 
 
 # 1. each
-    block.each {|item| print item}
-    block.my_each {|item| print item}
+    #  hash.each {|item| print item}
+    array_clone.my_each {|item| print item}
 
 # 2. each_with_index
 # test_array.each_with_index {|item,i| puts "Index: #{i} Item: #{item}"}
